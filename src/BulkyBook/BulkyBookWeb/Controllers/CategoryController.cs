98b5f82e4f2f9d34596e5b43c48c56e17af9c711
Kerminator-dev <pussy_hunter@list.ru>
2022-11-10 16:55:04 +0300

    Init

---
+++ src/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _dbContext.Categories.ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
---
+++ src/BulkyBook/BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }  
+    }
+}
