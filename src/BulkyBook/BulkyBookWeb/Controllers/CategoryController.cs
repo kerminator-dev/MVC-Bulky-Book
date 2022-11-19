fa4fae5a403386eab56315e62b1c46a225b5ab84
Kerminator <pussy_hunter@list.ru>
2022-11-19 18:22:05 +0300

    Добавлены CRUD методы и уведомления

--- src/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ src/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,11 +14,100 @@ namespace BulkyBookWeb.Controllers
             _dbContext = dbContext;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var categories = await _dbContext.Categories.ToListAsync();
 
             return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category Name should not be like a Display Order!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            await _dbContext.Categories.AddAsync(category);
+            await _dbContext.SaveChangesAsync();
+            TempData["Success"] = "Category created successfully";
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var category = await _dbContext.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category Name should not be like a Display Order!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            _dbContext.Categories.Update(category);
+            await _dbContext.SaveChangesAsync();
+            TempData["Success"] = "Category updated successfully";
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var category = await _dbContext.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }   
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteCategory(int? id)
+        {
+            var category = _dbContext.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Categories.Remove(category);
+            await _dbContext.SaveChangesAsync();
+            TempData["Success"] = "Category deleted successfully";
+
+            return RedirectToAction("Index");
         }
     }
 }
